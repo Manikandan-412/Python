2df7c3a3486e9569668ee903c6b5e57bc7326e55
Manikandan-412 <manikandansrinivasank@gmail.com>
2024-11-12 10:47:46 +0530

    first set commit

---
+++ strings.py
@@ -0,0 +1,31 @@
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+# upper case 
+upperString =  "hello world"
+upperString.upper()
+print(upperString)
+
+lowerString = "Hello, World!"
+print(lowerString.lower())
+
+# Slicing String
+
+b = "Hello, World!"
+print(b[2:5])
+
+# Remove Whitespace
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+# Replace String
+
+change = "Game over"
+print(change.replace("over", "begines"))
+
+# Split String
+= "Hello, World!"
+print(.split(",")) # returns ['Hello', ' World!']
---
+++ variables&Datatypes.py
@@ -0,0 +1,36 @@
+# python variables
+ 
+a = 4
+
+'''
+Naming rules 
+python was case sensitive
+cant start name with numbers
+only allowed charcters are (a-z, A-Z, 0-9, _)
+'''
+# Data types 
+
+name = "manikandan" # String
+
+age = 22    # integer
+
+hight = 5.8    # float
+
+subject = ['tamil','english','maths'] # list
+
+mark = { "tamil":67, "english":56, "maths":59 }   # dictionary
+
+colors = {"red", "green", "blue"}  # set
+
+fruits = ["apple", "banana", "cherry"] # list 
+
+
+# type Converion 
+
+x = int(1)      # normal
+y = int(2.8)    # float to int
+z = int("3")    # String to int 
+
+print(type(x)) # displayes the type
+print(y)
+print(z)
